57a63b549a3a4824bd5d6967088ed83c805cdf2f
BogiLoco <damian.ambrozkiewicz@gmail.com>
2024-08-03 08:42:15 +0200

    Adding test to call_request

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,7 +1,9 @@
-use crate::models::general::llm::{ Message };
+use crate::models::general::llm::{ Message, ChatCompletion };
 use dotenv::dotenv;
 use reqwest::Client;
 use std::env;
+
+use reqwest::header::{HeaderMap, HeaderValue};
 
 // Call Large Language Model
 pub async fn call_gpt(messages: Vec<Message>){
@@ -13,4 +15,48 @@ pub async fn call_gpt(messages: Vec<Message>){
 
     let url: &str = "https://api.openai.com/v1/chat/completions";
 
+    // Create headers
+    let mut headers = HeaderMap::new();
+
+    // Create api key header
+    headers.insert("authorization", HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap());
+
+    // Create Open AI Org header
+    headers.insert("OpenAI-Organization", HeaderValue::from_str(&api_org.as_str()).unwrap());
+
+    // Create client
+    let client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+
+    // Create chat completion 
+    let chat_completion = ChatCompletion {
+        model: "gpt-3.5-turbo".to_string(),
+        messages,
+        temperature: 0.1
+    };
+
+    // Troubleshooting
+    let res_raw = client.post(url).json(&chat_completion).send().await.unwrap();
+    
+    dbg!(res_raw.text().await.unwrap());
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_call_to_openai() {
+        let message = Message {
+            role: "user".to_string(),
+            content: "What is the weather in Thailand?".to_string(),
+        };
+
+        let messages = vec!(message);
+
+        call_gpt(messages).await;
+    }
 }
--- src/models/general/llm.rs
+++ src/models/general/llm.rs
@@ -5,3 +5,10 @@ pub struct Message {
     pub role: String, 
     pub content: String
 }
+
+#[derive(Debug, Serialize, Clone)]
+pub struct ChatCompletion {
+    pub model: String, 
+    pub messages: Vec<Message>,
+    pub temperature: f32,
+}
