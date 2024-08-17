dd53a5e42e8e433f826b1f95edf464f836888b4c
BogiLoco <damian.ambrozkiewicz@gmail.com>
2024-08-17 18:19:07 +0200

    Adding ai request function

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,4 +1,10 @@
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
+
+use crate::apis::call_request::call_gpt;
+use crate::models::general::llm;
 use crate::models::{self, general::llm::Message};
+use crate::helpers::command_line::PrintCommand;
 
 // Extend ai function to encourage certain specific output
 pub fn extend_ai_function(ai: fn(&str) -> &'static str, func_input: &str) -> Message{
@@ -15,6 +21,46 @@ pub fn extend_ai_function(ai: fn(&str) -> &'static str, func_input: &str) -> Mes
         role: "system".to_string(),
         content: msg
      }
+
+}
+
+ // Performs call to LLM GPT
+pub async fn ai_task_request(
+   msg_context: String, 
+   agent_position: &str, 
+   agent_operation: &str, 
+   function_pass: for<'a> fn(&'a str) -> &'static str
+) -> String {
+
+
+   // Extend AI function
+   let extended_msg: Message = extend_ai_function(function_pass, &msg_context);
+
+   // Print current status
+   PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+   // Get LLM response
+   let llm_response_res = call_gpt(vec![extended_msg.clone()]).await;
+
+   // Return success or try again
+   match llm_response_res {
+       Ok(llm_resp) => llm_resp,
+       Err(_) => call_gpt(vec![extended_msg.clone()]).await.expect("Failed twice to call OpenAI")
+   }
+}
+
+ // Performs call to LLM GPT - decoded
+ pub async fn ai_task_request_decoded<T: DeserializeOwned>(
+   msg_context: String, 
+   agent_position: &str, 
+   agent_operation: &str, 
+   function_pass: for<'a> fn(&'a str) -> &'static str
+) -> T {
+   
+   let llm_response = ai_task_request(msg_context, agent_position, agent_operation, function_pass).await;
+   let decoded_response: T = serde_json::from_str(llm_response.as_str())
+      .expect("Failed to decode ai response from serde_json");
+   return decoded_response;
 }
 
 #[cfg(test)]
@@ -27,5 +73,19 @@ mod tests {
        let extended_msg = extend_ai_function(convert_user_input_to_goal, "dummy variable");
        dbg!(&extended_msg);
        assert_eq!(extended_msg.role, "system".to_string());
+    }
+
+    #[tokio::test]
+    async fn tests_ai_task_request(){
+
+      let ai_func_param = "Build me a webserver for making stock price api requests.".to_string();
+      let res = ai_task_request(
+         ai_func_param,
+         "Managing Agent",
+         "Definig user requirements",
+         convert_user_input_to_goal
+      ).await;
+
+      assert!(res.len()>20);
     }
 }
